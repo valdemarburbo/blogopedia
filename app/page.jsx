f3bd89d4ccf72489ad70627bc7fcc19eb93adbe9
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-08-24 19:46:55 +0100

    Component structure finished

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,16 @@
 const Home = () => {
   return (
         <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Discover & Share
+                <br />
+                <span className="orange_gradient text-center">AI-Generated Blog Posts</span>
+            </h1>
+            <p className="desc text-center">
+                Blogopedia is an open-source library of AI generated blog posts for everyone to enjoy.
+            </p>
 
+            {/* Feed */}
         </section>
   )
 }

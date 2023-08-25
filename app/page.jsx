a619108d245c604f08f1b261cc8de763462f7006
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-08-25 02:29:37 +0100

    Create post functionality finished

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ const Home = () => {
             <h1 className="head_text text-center">
                 Discover & Share
                 <br />
-                <span className="orange_gradient text-center">AI-Generated Blog Posts</span>
+                <span className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text text-center">AI-Generated Blog Posts</span>
             </h1>
             <p className="desc text-center">
                 Blogopedia is an open-source library of AI generated blog posts for everyone to enjoy.
